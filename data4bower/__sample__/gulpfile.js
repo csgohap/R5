70af674616483653ef2f586d35027d659adcb934
4gekkman <4gekkman@gmail.com>
2016-04-02 01:37:00 +0300

    Autosave to

--- data4bower/__sample__/gulpfile.js
+++ data4bower/__sample__/gulpfile.js
@@ -1,66 +1,88 @@
-'use strict';
+//------------------------------------------//
+//                                          //
+//        GulpFile.js bower-пакета					//
+//                                          //
+//------------------------------------------//
+//	Оглавление  //
+//--------------//
+/*
 
-// 1] Подключить модули
+	1. Подключить необходимые NPM-пакеты
+	2. Создать файл с датой последнего исполнения рядом с gulpfile.js
+	3. ...
+
+	x. Выполнить все необходимые задачи этого gulpfile
+	n. Примеры часто используемых задач
+
+		▪ Обработать SCSS-исходники
+		▪ Скопировать всё из assets в public
+		▪ Параллельно выполнить задачи styles и assets
+
+*/
+//------------------------------------------//
+//	Код  //
+//-------//
+
+// 1. Подключить необходимые NPM-пакеты
+'use strict';
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const watch = require('gulp-watch');
+const file = require('gulp-file');
+const fs = require('fs');
 const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
-const sleep = require('sleep');
 
-// 2] Задача №1: Обработка scss-файлов
-gulp.task('styles', function(callback){
+// 2. Создать файл с датой последнего исполнения рядом с gulpfile.js
+gulp.task('lastuse', function(callback) {
 
-	// 3.1] Найти и обработать .scss файлы, записать в public
-	return gulp.src('frontend/styles/main.scss')
-			.pipe(sourcemaps.init())
-			.pipe(sass())
-			.pipe(sourcemaps.write())
-			.pipe(gulp.dest('public'));
+	console.log(__dirname.split("/").pop());
 
-	// 3.2] Сигнализировать о завершении задачи
-	//callback();
+	fs.writeFile("lastuse", "Hey there!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+	}); 
+
+  callback();
 
 });
 
-// 3] Задача №2: копирует всё из assets в public
-gulp.task('assets', function(){
-	return gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')})
-			.pipe(gulp.dest('public'));
-});
 
-// 5] Задача №3:
-// - Параллельно выполняет задачи styles и assets
-gulp.task('build', gulp.series(
-	gulp.parallel('styles', 'assets')
+// x. Выполнить все необходимые задачи этого gulpfile
+gulp.task('run', gulp.series(
+	gulp.parallel('lastuse')
 ));
 
-// 6] Задача №4: следить за файлами, запускать задачу при их изменении
-gulp.task('watch', function(){
-	gulp.watch('frontend/styles/**/*.*', {usePolling: true}, gulp.series('styles'));
-	gulp.watch('frontend/assets/**/*.*', {usePolling: true}, gulp.series('assets'));
-});
 
-// 7] Задача №6: запустить сервер browser-sync
-gulp.task('serve', function(){
+// n. Примеры часто используемых задач
 
-	// 7.1] Запустить proxy
-	browserSync.init({
-		server: "public",
-		port: 3000,
-		ui: {
-		  port: 3001
-		}		
-	});
 
-	// 7.2] Отслеживать изменения в указанных файлах
-	browserSync.watch('public/*.css', {usePolling: true}).on('change', browserSync.reload);
+	// Обработать SCSS-исходники
+	// gulp.task('styles', function(callback){
 
-});
+	// 	// Найти и обработать .scss файлы, записать в public
+	// 	return gulp.src('frontend/styles/main.scss')
+	// 			.pipe(sourcemaps.init())
+	// 			.pipe(sass())
+	// 			.pipe(sourcemaps.write())
+	// 			.pipe(gulp.dest('public'));
 
-// 8] Задача №5: сначала запустить build, затем параллельно watch и serve
-gulp.task('dev',
-		gulp.series('build', gulp.parallel('watch', 'serve')));
+	// 	// Сигнализировать о завершении задачи
+	// 	//callback();
 
+	// });
+
+
+	// Скопировать всё из assets в public
+	// gulp.task('assets', function(){
+	// 	return gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')})
+	// 			.pipe(gulp.dest('public'));
+	// });
+
+
+	// Параллельно выполнить задачи styles и assets
+	// gulp.task('build', gulp.series(
+	// 	gulp.parallel('styles', 'assets')
+	// ));
 
 
